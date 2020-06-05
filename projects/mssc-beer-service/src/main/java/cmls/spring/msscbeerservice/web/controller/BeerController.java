785c459e85af9923f8e139879baf0012de6d7c54
Carlos Lozano <carlosmlozanos@gmail.com>
2020-06-05 09:16:01 +0200

    Add class validation for method params

--- projects/mssc-beer-service/src/main/java/cmls/spring/msscbeerservice/web/controller/BeerController.java
+++ projects/mssc-beer-service/src/main/java/cmls/spring/msscbeerservice/web/controller/BeerController.java
@@ -4,11 +4,13 @@ import java.net.URI;
 import java.util.UUID;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import cmls.spring.msscbeerservice.web.model.Beer;
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/beers")
 public class BeerController {
@@ -27,20 +30,20 @@ public class BeerController {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Beer> findById(@PathVariable UUID id) {
+	public ResponseEntity<Beer> findById(@NotNull @PathVariable UUID id) {
 		logger.info("findById");
 		return ResponseEntity.ok(Beer.builder().id(UUID.randomUUID()).build());
 	}
 
 	@PostMapping
-	public ResponseEntity<Beer> save(@Valid @RequestBody Beer beer) {
+	public ResponseEntity<Beer> save(@Valid @NotNull @RequestBody Beer beer) {
 		logger.info("save");
 		return ResponseEntity.created(URI.create("/api/v1/beers/" + UUID.randomUUID())).build();
 	}
 	
 	@PutMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void update(@PathVariable UUID id,@Valid @RequestBody Beer beer) {
+	public void update(@NotNull @PathVariable UUID id,@NotNull @Valid @RequestBody Beer beer) {
 		logger.info("update");
 	}
 	
